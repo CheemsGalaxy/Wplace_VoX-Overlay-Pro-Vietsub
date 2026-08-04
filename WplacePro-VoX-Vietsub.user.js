73c320fff1dea452b0f160eb45c648daba23dc83
CheemsGalaxy <149580222+CheemsGalaxy@users.noreply.github.com>
2026-08-04 19:52:02 +0700

    Update WplacePro-VoX-Vietsub.user.js

--- WplacePro-VoX-Vietsub.user.js
+++ WplacePro-VoX-Vietsub.user.js
@@ -4,6 +4,8 @@
 // @version      5.3.0-vi
 // @description  [Vietsub by CheemsGalaxy] Overlays tiles on wplace.live. Can also resize, and color-match your overlay to wplace's palette. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under GPLv3.
 // @author       shinkonet (Modificado por @SrCratier), Vietsub by CheemsGalaxy
+// @updateURL    https://raw.githubusercontent.com/CheemsGalaxy/Wplace_VoX-Overlay-Pro-Vietsub/main/WplacePro-VoX.user.js
+// @downloadURL  https://raw.githubusercontent.com/CheemsGalaxy/Wplace_VoX-Overlay-Pro-Vietsub/main/WplacePro-VoX.user.js
 // @match        https://wplace.live/*
 // @license      GPLv3
 // @grant        GM_setValue
